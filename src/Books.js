d0a32ba37dcb79b5551c92012ded3022eae65852
Eric-Andresen <erandre7@gmail.com>
2018-05-07 12:40:12 -0700

    Display all book data

--- src/Books.js
+++ src/Books.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
+const FabStatusChange = () => (
+    <button>Button</button>
+)
+
 const Books = (props) => {
     const names = props.books.map( (book, i) => (
         <li 
             key = {i} 
             onClick = {() => console.log(`Go to ${book.title} focus`)} >
-            {book.title}
+            <img height = "200px" src = {book.imgUrl} />
+            <h4>{book.title}</h4>
+            <h5>{book.author}</h5>
+            <FabStatusChange book = {book}/>
         </li>
     ))
 
