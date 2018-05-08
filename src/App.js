9d1445375bcf0d2e74e519b14b932df366321562
Eric-Andresen <erandre7@gmail.com>
2018-05-08 14:48:18 -0700

    Make images unique

--- src/App.js
+++ src/App.js
@@ -39,42 +39,42 @@ const initialState = {
         title: "A farewell to Arms",
         author: "Ernest Hemingway",
         status: "currently-reading",
-        imgUrl: "https://99designs-blog.imgix.net/wp-content/uploads/2017/12/attachment_83090027.jpg?auto=format&q=60&fit=max&w=930"
+        imgUrl: "https://insiders.bookriot.com/wp-content/uploads/2017/07/They_Both_Die_at_the_End_cover.jpg",
       },
       {
         index: 4,
         title: "A short introduction to blood",
         author: "Ernest Hemingway",
         status: "want-to-read",
-        imgUrl: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/art_bookcover.png"
+        imgUrl: "https://i.pinimg.com/736x/1c/52/59/1c5259ef6eb54ce4bfc2c1880fdaaa90.jpg"
       },
       {
         index: 5,
         title: "Titan",
         author: "Ernest Hemingway",
         status: "done-reading",
-        imgUrl: "https://static1.squarespace.com/static/5202d1b3e4b099a0812c51a3/523753d6e4b030ffaec4a565/5a2043310d9297af42dde909/1512063793878/Dungeon-Lord.jpg"
+        imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV6FcQwPboXhx4lvX4RYM4i_y_MGYmV_QqCQPeuqCFXs_fAwC3Hw"
       },
       {
         index: 6,
         title: "A farewell to Arms",
         author: "Ernest Hemingway",
         status: "currently-reading",
-        imgUrl: "https://99designs-blog.imgix.net/wp-content/uploads/2017/12/attachment_83090027.jpg?auto=format&q=60&fit=max&w=930"
+        imgUrl: "http://www.designpointinc.com/wp-content/uploads/designpoint-book-cover-mom-im-gay.jpg"
       },
       {
         index: 7,
         title: "A short introduction to blood",
         author: "Ernest Hemingway",
         status: "want-to-read",
-        imgUrl: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/art_bookcover.png"
+        imgUrl: "https://www.poetrysociety.org/psa/poetry/crossroads/on_poetry/covers/attachment/preview.jpg"
       },
       {
         index: 8,
         title: "Titan",
         author: "Ernest Hemingway",
         status: "done-reading",
-        imgUrl: "https://static1.squarespace.com/static/5202d1b3e4b099a0812c51a3/523753d6e4b030ffaec4a565/5a2043310d9297af42dde909/1512063793878/Dungeon-Lord.jpg"
+        imgUrl: "http://wwwcdn.printmag.com/wp-content/uploads/1984-book-covers-2.jpg",
       },
     ],
 }
