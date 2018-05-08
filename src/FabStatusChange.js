19e07bf735024a48730b47a93f643586167c7f39
Eric-Andresen <erandre7@gmail.com>
2018-05-08 14:20:18 -0700

    Toggle Options on hover

--- src/FabStatusChange.js
+++ src/FabStatusChange.js
@@ -9,28 +9,49 @@ class FabStatusChange extends Component {
     constructor(props) {
         super(props);
         this.book = this.props.book;
-        this.showList = false;
+        this.state = {
+            isHidden : true
+        }
     }
 
     updateBook (status) {
         return () => this.props.dispatch({type: "UPDATE_BOOK", index: this.book.index, status})
     }
 
+    showOptions() {
+        this.setState({
+            isHidden: false
+        })
+    }
+
+    hideOptions() {
+        this.setState({
+            isHidden : true
+        })
+    }
+
+    options = (<select 
+        id = "status-menu" size = "3"
+        // can replace this with short circuit operator
+        onMouseOver = {() => this.showOptions()}
+        onMouseLeave = {() => this.hideOptions()}>
+        <option value ="done-reading">Done Reading</option>
+        <option value ="want-to-read">Want to Read</option>
+        <option value ="currently-reading">Currently Reading</option>
+    </select>)
+
     render() {
         return ( 
-            <div>
+            <div
+                onMouseOver = {() => this.showOptions()}
+                onMouseLeave = {() => this.hideOptions()}>
                 <button 
                     className = "fab"
-                    onClick = {this.updateBook("done-reading")} >
+                    onClick = {this.updateBook("done-reading")} 
+                    >
                     <i className="material-icons">keyboard_arrow_down</i>
                 </button>
-                <select 
-                    id = "status-menu" size = "3"
-                    className = {this.showList ? "show" : "hide"}>
-                    <option value ="done-reading">Done Reading</option>
-                    <option value ="want-to-read">Want to Read</option>
-                    <option value ="currently-reading">Currently Reading</option>
-                </select>
+                {!this.state.isHidden && this.options}
             </div>
         )
     }
